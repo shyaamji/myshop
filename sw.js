1030a5afb00d8343ff97bc5df087117649868d7e
shyaamji <144800474+shyaamji@users.noreply.github.com>
2026-07-03 03:11:57 +0630

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'kaungmyat-pos-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://unpkg.com/html5-qrcode'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
